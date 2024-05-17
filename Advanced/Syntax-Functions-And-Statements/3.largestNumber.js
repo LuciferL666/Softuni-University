5778cb99e4a91de729d239fb886f948da449416e
PeykoGeorgiev <peyko92gm@gmail.com>
2024-05-17 10:51:04 +0200

    Update 3.largestNumber.js

--- Advanced/Syntax-Functions-And-Statements/3.largestNumber.js
+++ Advanced/Syntax-Functions-And-Statements/3.largestNumber.js
@@ -2,5 +2,5 @@ function max (one, two, three) {
     result = Math.max(one, two, three)
     console.log(`The largest number is ${result}.`);
 }
-max(5, -3, 16 )
-max(-3, -5, -22.5)
+max(5, -3, 16 );
+max(-3, -5, -22.5);
