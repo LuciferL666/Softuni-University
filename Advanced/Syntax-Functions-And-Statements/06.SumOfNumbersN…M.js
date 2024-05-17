be85e8551d897387f1e98fd9b7ca9ded3d139173
PeykoGeorgiev <peyko92gm@gmail.com>
2024-05-17 10:51:20 +0200

    Create 06.SumOfNumbersN…M.js

---
+++ Advanced/Syntax-Functions-And-Statements/06.SumOfNumbersN…M.js
@@ -0,0 +1,12 @@
+function solve(n, m){
+    let number1 = Number(n);
+    let number2 = Number(m);
+
+    let result = 0;
+    for (let i = number1; i <= number2; i++){
+        result += i;
+    }
+    console.log(result);
+}
+solve('1', '5')
+solve(-8, 20)
