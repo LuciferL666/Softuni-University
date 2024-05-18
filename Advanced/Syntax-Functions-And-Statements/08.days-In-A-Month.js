deb05179c4d099a2055a60f1fc2d0629132f95d3
PeykoGeorgiev <peyko92gm@gmail.com>
2024-05-18 22:47:31 +0200

    Update 08.days-In-A-Month.js

--- Advanced/Syntax-Functions-And-Statements/08.days-In-A-Month.js
+++ Advanced/Syntax-Functions-And-Statements/08.days-In-A-Month.js
@@ -1,7 +1,4 @@
 function days (month, year){
-    return new Date(year, month, 0).getDate();
-    
+    return new Date(year, month, 0).getDate();   
 }
-let result = days(month, year);
-console.log(result);
 days(1, 2015)
