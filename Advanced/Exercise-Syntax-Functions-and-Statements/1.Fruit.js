a28f9d8ea0ef8e43035f270cf0c881c1a0b7c122
PeykoGeorgiev <peyko92gm@gmail.com>
2024-05-20 14:45:09 +0200

    Update 1.Fruit.js

--- Advanced/Exercise-Syntax-Functions-and-Statements/1.Fruit.js
+++ Advanced/Exercise-Syntax-Functions-and-Statements/1.Fruit.js
@@ -4,4 +4,4 @@ function fruit (type, weight, price){
     let totalMoney = weightInKg * price;
     console.log(`I need $${totalMoney.toFixed(2)} to buy ${weightInKg.toFixed(2)} kilograms ${type}.`);
 }
-fruit('orange', 2500, 1.80)
+fruit('orange', 2500, 1.80);
