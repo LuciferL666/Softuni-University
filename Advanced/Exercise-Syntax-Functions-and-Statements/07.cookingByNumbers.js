0f3016714cac52bea5ad8e05ea1319e97ce05c4e
PeykoGeorgiev <peyko92gm@gmail.com>
2024-05-20 15:55:48 +0200

    Create 07.cookingByNumbers.js

---
+++ Advanced/Exercise-Syntax-Functions-and-Statements/07.cookingByNumbers.js
@@ -0,0 +1,16 @@
+function cookingByNumbers(number, ...commands) {
+    number = Number(number);
+    const operations = {
+        chop() { number = number / 2;},
+        dice() { number = Math.sqrt(number);},
+        spice() { number++;},
+        bake() { number *= 3;},
+        fillet() { number *= 0.8},
+    };
+    for(const command of commands) {
+        operations[command]();
+        console.log(number);
+    };
+};
+
+cookingByNumbers('32', 'chop', 'chop', 'chop', 'chop', 'chop' )
