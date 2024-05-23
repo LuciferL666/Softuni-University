448509bfd8c90a2d9343b905d90b4a647b3d59eb
PeykoGeorgiev <peyko92gm@gmail.com>
2024-05-23 23:38:50 +0200

    Create 02.printEveryN-thElementFromAnArray.js

---
+++ Advanced/Exercise Arrays and Nested Arrays/02.printEveryN-thElementFromAnArray.js
@@ -0,0 +1,34 @@
+function printEveryNthElement(arr, step){
+    result = [];
+
+    for(let i = 0; i< arr.length; i+= step){
+        result.push(arr[i])
+    }
+    console.log(result);
+    // return result
+}
+
+printEveryNthElement(
+['5',
+'20',
+'31',
+'4',
+'20'],
+2 )
+
+printEveryNthElement(
+    ['dsa',
+    'asd',
+    'test',
+    'tset'],
+    2
+)
+
+printEveryNthElement(
+    ['1', 
+    '2', 
+    '3', 
+    '4', 
+    '5'],
+    6
+)
