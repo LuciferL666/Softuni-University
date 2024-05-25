d0aab1763dc5608afc6913af459e77429521f7f1
PeykoGeorgiev <peyko92gm@gmail.com>
2024-05-25 22:20:25 +0200

    Create 09.magicMatrices.js

---
+++ Advanced/Exercise Arrays and Nested Arrays/09.magicMatrices.js
@@ -0,0 +1,16 @@
+function magicMatrices(arr1, arr2, arr3) {
+
+    sum1 = 0
+    sum2 = 0
+    sum3 = 0
+
+for (let el of arr1) {
+    sum1 = el
+}
+console.log(sum1);
+}
+
+magicMatrices(
+[[4, 5, 6],
+[6, 5, 4],
+[5, 5, 5]])
