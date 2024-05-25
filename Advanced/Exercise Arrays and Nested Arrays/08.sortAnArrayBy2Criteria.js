87a8776cb43d1ea7f9692c55a4716b171a1ddbb8
PeykoGeorgiev <peyko92gm@gmail.com>
2024-05-25 22:54:58 +0200

    Create 08.sortAnArrayBy2Criteria.js

---
+++ Advanced/Exercise Arrays and Nested Arrays/08.sortAnArrayBy2Criteria.js
@@ -0,0 +1,20 @@
+function sortBy2Criteria(arr) {
+    let sortArr = arr.sort((a, b) => {
+        if(a.length !== b.length){
+            return a.length - b.length;
+        }else {
+            return a.localeCompare(b);
+        }
+    })
+    console.log(sortArr.join('\n'));
+}
+
+sortBy2Criteria(
+['alpha',
+'beta',
+'gamma'] 
+)
+
+sortBy2Criteria(
+    ['Isacc', 'Theodor', 'Jack', 'Harrison', 'George']
+)
