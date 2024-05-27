720e55e2f414b9b04b34e01e58e755fab8d8cbb4
PeykoGeorgiev <peyko92gm@gmail.com>
2024-05-27 22:26:19 +0200

    Create 01.cityRecord.js

---
+++ Advanced/Lab-Objects & Composition/01.cityRecord.js
@@ -0,0 +1,11 @@
+function cityRecords(city, population, treasury){
+    result = {
+        name: city,
+        population: population,
+        treasury: treasury,
+    };
+    return result;
+}
+console.log(cityRecords('Tortuga',
+7000,
+15000 ));
