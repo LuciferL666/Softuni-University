54a2159e4452f96163a3e1806681367915ca11f9
PeykoGeorgiev <peyko92gm@gmail.com>
2024-06-02 20:34:45 +0200

    Create 08.rectangle.js

---
+++ Advanced/Exercises Objects & Composition/08.rectangle.js
@@ -0,0 +1,20 @@
+function rectangle(width, height, color){
+    result = {
+        width,
+        height,
+        color: color[0].toUpperCase() + color.substring(1),
+        calcArea: function(){
+            return this.width * this.height 
+        }
+    }
+    return result
+}
+let rect = rectangle(4, 5, "red");
+
+console.log(rect.width);
+
+console.log(rect.height);
+
+console.log(rect.color);
+
+console.log(rect.calcArea());
