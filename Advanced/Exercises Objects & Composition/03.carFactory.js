69ba8719193d16ea1f9e27d8190ab83f51d364dd
PeykoGeorgiev <peyko92gm@gmail.com>
2024-06-02 20:34:37 +0200

    Create 03.carFactory.js

---
+++ Advanced/Exercises Objects & Composition/03.carFactory.js
@@ -0,0 +1,57 @@
+function carFactory(data) {
+    let result = {};
+    result.model = data.model;
+
+    if(data.power <= 90) {
+        result.engine = {
+            power: 90,
+            volume: 1800
+        }
+    }else if (data.power <= 120){
+        result.engine = {
+            power: 120,
+            volume: 2400
+        }
+    }else if (data.power <= 200){
+        result.engine = {
+            power: 200,
+            volume: 3500
+        }
+    }
+
+    if(data.carriage  === 'hatchback'){
+        result.carriage = {
+            type: "hatchback",
+            color: data.color
+        }
+    }else {
+        result.carriage = {
+            type: "coupe",
+            color: data.color
+        } 
+    }   
+        let size;
+    if(data.wheelsize % 2 != 0){
+        size = data.wheelsize
+    }else {
+        size = data.wheelsize - 1;
+    }
+    result.wheels = [size, size, size, size]
+    
+    //console.table(result)
+
+    return result
+}
+
+carFactory({ model: 'VW Golf II',
+power: 90,
+color: 'blue',
+carriage: 'hatchback',
+wheelsize: 14 })
+
+
+carFactory({ model: 'Opel Vectra',
+power: 110,
+color: 'grey',
+carriage: 'coupe',
+wheelsize: 17 })
