c2a67010141102312cf4e9dd4d254b43aa3a37d6
PeykoGeorgiev <peyko92gm@gmail.com>
2024-06-07 21:36:19 +0200

    Create ShowMore.js

---
+++ Advanced/DOM Introduction/01.Edit Element/04.Show More/ShowMore.js
@@ -0,0 +1,14 @@
+function showText() {
+    const text = document.getElementById('text')
+    text.style.display = 'inline'
+
+    const btn = document.getElementById('more')
+    btn.style.display = 'none'
+}
+
+//second
+// function showText() {
+//     const text = document.getElementById('text').style.display = 'inline'
+
+//     const btn = document.getElementById('more').style.display = 'none'
+// }
