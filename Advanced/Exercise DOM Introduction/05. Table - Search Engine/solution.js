e934c7fe6c9509ef48a99aa372105f0b847f8904
PeykoGeorgiev <peyko92gm@gmail.com>
2024-06-11 22:52:28 +0200

    Update solution.js

--- Advanced/Exercise DOM Introduction/05. Table - Search Engine/solution.js
+++ Advanced/Exercise DOM Introduction/05. Table - Search Engine/solution.js
@@ -1,8 +1,17 @@
 function solve() {
    document.querySelector('#searchBtn').addEventListener('click', onClick);
 
-   function onClick() {
-      //   TODO:
+   const tdList = Array.from(document.querySelectorAll('tr'));
 
+   let input = document.getElementById('searchField');
+   
+   function onClick() {
+      for(const item of tdList) {
+         item.classList.remove('select');
+         if(item.innerHTML.includes(input.value)) {
+            item.className = 'select';
+         }
+      }
+      input.value = '';
    }
 }
