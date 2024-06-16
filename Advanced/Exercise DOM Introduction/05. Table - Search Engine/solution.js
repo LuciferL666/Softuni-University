6f42f4b54e0df49bad8601b7e2eb2e546369de72
PeykoGeorgiev <peyko92gm@gmail.com>
2024-06-16 20:16:55 +0200

    Update solution.js

--- Advanced/Exercise DOM Introduction/05. Table - Search Engine/solution.js
+++ Advanced/Exercise DOM Introduction/05. Table - Search Engine/solution.js
@@ -6,10 +6,10 @@ function solve() {
    let input = document.getElementById('searchField');
    
    function onClick() {
-      for(const item of tdList) {
+      for(const item of tdList){
          item.classList.remove('select');
-         if(item.innerHTML.includes(input.value)) {
-            item.className = 'select';
+         if(item.innerHTML.includes(input.value)){
+            item.className = 'select'
          }
       }
       input.value = '';
