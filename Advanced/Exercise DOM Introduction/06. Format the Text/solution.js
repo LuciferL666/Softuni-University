5403fb936eca4803d90e396bbc6154ebe83c3c2a
PeykoGeorgiev <peyko92gm@gmail.com>
2024-06-16 20:27:10 +0200

    Update solution.js

--- Advanced/Exercise DOM Introduction/06. Format the Text/solution.js
+++ Advanced/Exercise DOM Introduction/06. Format the Text/solution.js
@@ -15,3 +15,19 @@ function solve() {
     output.innerHTML += `<p>${resText}</p>`
   }
 }
+
+
+//other solution with while
+// function solve() {
+//   let input = document.getElementById('input').value;
+//   let output = document.getElementById('output');
+
+//   let sentance = input.split('.').filter((s) => s.length !== 0);
+
+//   while (sentance.length > 0){
+//     let textParagraph = sentance.splice(0, 3).join('. ') + '.';
+//     let p = document.createElement('p');
+//     p.textContent = textParagraph
+//     output.appendChild(p)
+//   }
+// }
